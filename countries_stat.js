b10712fdfb40d9cd171688aa7dd53f43a5ce18a3
chkim116 <66462726+chkim116@users.noreply.github.com>
2020-07-25 19:36:27 +0900

    코로나 전세계 현황
    새로고침 시 업데이트 됩니다.

---
+++ countries_stat.js
@@ -0,0 +1,52 @@
+function loadCorona() {
+  fetch(
+    "https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php",
+    {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
+        "x-rapidapi-key": "53009286a0mshdc8ec356f7aa205p1e0e80jsn5858f548ed53",
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then((stat) => {
+      let countriesStat = stat.countries_stat;
+      let coronaStat = document.querySelector(".corona_stat");
+      coronaStat.innerHTML = countriesStat
+        .map((item) => {
+          return `<div class="corona_conutries">${item.country_name}</div>
+          <div class="corona_case">${item.cases}명</div>`;
+        })
+        .join("");
+      atTime(stat);
+    })
+
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+loadCorona();
+
+function atTime(stat) {
+  let time = new Date();
+  let Year = time.getFullYear();
+  let month = time.getMonth();
+  let day = time.getDay();
+  let hour = time.getHours();
+  let minute = time.getMinutes();
+  let seconds = time.getSeconds();
+
+  let date = `${Year}-${month < 10 ? `0${month}` : `${month}`}-${
+    day < 10 ? `0${day}` : `${day}`
+  } ${hour < 10 ? `0${hour}` : `${hour}`}:${
+    minute < 10 ? `0${minute}` : `${minute}`
+  }:${seconds < 10 ? `0${seconds}` : `${seconds}`}`;
+  let atTime = document.querySelector(".at_time");
+  atTime.innerText = date;
+}
+
+setInterval(atTime, 1000);
+
+//https://doctorcodetutorial.blogspot.com/2020/03/create-coronavirus-tracker-in.html 출저
