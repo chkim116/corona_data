db50f19e75296f53e51325e725aca7a0d427554a
chkim116 <66462726+chkim116@users.noreply.github.com>
2020-07-26 00:23:16 +0900

    자바스크립트 배분

--- countries_stat.js
+++ countries_stat.js
@@ -11,26 +11,6 @@ function coronaStat(stat) {
   coronaStat.innerHTML = coronaStatHTML;
 }
 
-function atTime() {
-  let time = new Date();
-  let Year = time.getFullYear();
-  let month = time.getMonth() + 1;
-  let day = time.getDate();
-  let hour = time.getHours();
-  let minute = time.getMinutes();
-  let seconds = time.getSeconds();
-  let date = `${Year}-${month < 10 ? `0${month}` : `${month}`}-${
-    day < 10 ? `0${day}` : `${day}`
-  } ${hour < 10 ? `0${hour}` : `${hour}`}:${
-    minute < 10 ? `0${minute}` : `${minute}`
-  }:${seconds < 10 ? `0${seconds}` : `${seconds}`}`;
-  console.log(day);
-
-  let atTime = document.querySelector(".at_time");
-
-  return (atTime.innerText = date);
-}
-
 function loadCorona() {
   return fetch(
     "https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php",
