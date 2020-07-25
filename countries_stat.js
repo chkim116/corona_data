7a0b4b42f241b19c01da6bdcd2ddf996199b4d81
chkim116 <66462726+chkim116@users.noreply.github.com>
2020-07-26 01:03:59 +0900

    Update countries_stat.js

--- countries_stat.js
+++ countries_stat.js
@@ -1,16 +1,3 @@
-function coronaStat(stat) {
-  let countriesStat = stat.countries_stat;
-  let coronaStat = document.querySelector(".corona_stat");
-  let coronaStatHTML = countriesStat
-    .map((item) => {
-      return `<div class="corona_conutries">${item.country_name}</div>
-    <div class="corona_case">${item.cases}명</div>`;
-    })
-    .join("");
-  setInterval(atTime, 1000);
-  coronaStat.innerHTML = coronaStatHTML;
-}
-
 function loadCorona() {
   return fetch(
     "https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php",
@@ -32,6 +19,19 @@ function loadCorona() {
     });
 }
 
-loadCorona();
+function coronaStat(stat) {
+  let countriesStat = stat.countries_stat;
+  let coronaStat = document.querySelector(".corona_stat");
+  let coronaStatHTML = countriesStat
+    .map((item) => {
+      return `<div class="corona_conutries">${item.country_name}</div>
+    <div class="corona_case">${item.cases}명</div>`;
+    })
+    .join("");
+  atTime();
+  setInterval(atTime, 1000);
+  coronaStat.innerHTML = coronaStatHTML;
+}
 
-//https://doctorcodetutorial.blogspot.com/2020/03/create-coronavirus-tracker-in.html 출저
+loadCorona();
+//https://doctorcodetutorial.blogspot.com/2020/03/create-coronavirus-tracker-in.html 출처
